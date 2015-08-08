7d5c6717b087c45abb203ae5b4e5f25ab6974fb4
Martijn Vermaat <martijn@vermaat.name>
2015-08-08 10:06:20 +0200

    Fix tests runner
    When specifying `-o pipefail`, the pipe to `grep -q` might fail since
    it would not consume the entire stream.

--- tests/run.sh
+++ tests/run.sh
@@ -9,11 +9,11 @@ ansible-playbook -i inventory.yml playbook.yml --syntax-check
 ansible-playbook -i inventory.yml playbook.yml --connection=local
 
 ansible-playbook -i inventory.yml playbook.yml --connection=local \
-  | grep -q 'changed=0.*failed=0' \
+  | grep 'changed=0.*failed=0' > /dev/null \
   && (echo 'Idempotence test: pass' && exit 0) \
   || (echo 'Idempotence test: fail' && exit 1)
 
 curl -s -k https://localhost/api/ \
-  | grep -q '"status".*:.*"ok"' \
+  | grep '"status".*:.*"ok"' > /dev/null \
   && (echo 'Varda API test: pass' && exit 0) \
   || (echo 'Varda API test: fail' && exit 1)
